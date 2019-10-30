9ca590a7c4437651881d81c256bb0ab58e154e0b
The Great Aurekal <aurek@parkcitiesxc.org>
2019-10-30 14:25:09 -0500

    DB Setup: MySQL - Local

--- config/database.php
+++ config/database.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mysql_local'),
+    // 'default' => env('DB_CONNECTION', 'mysql_remote'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,15 +44,35 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
+        'mysql_local' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => env('DBL_HOST', '127.0.0.1'),
+            'port' => env('DBL_PORT', '3306'),
+            'database' => env('DBL_DATABASE', 'forge'),
+            'username' => env('DBL_USERNAME', 'forge'),
+            'password' => env('DBL_PASSWORD', ''),
+            'unix_socket' => env('DBL_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'mysql_remote' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DBR_HOST', '127.0.0.1'),
+            'port' => env('DBR_PORT', '3306'),
+            'database' => env('DBR_DATABASE', 'forge'),
+            'username' => env('DBR_USERNAME', 'forge'),
+            'password' => env('DBR_PASSWORD', ''),
+            'unix_socket' => env('DBR_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
